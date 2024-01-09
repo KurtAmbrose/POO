fdc70a8f28d96e24196f1d3f7349ab91739d6f5f
Diego Bravo <A2270193@correo.uia.mx>
2024-01-09 17:45:00 -0600

    #Primer programa de la clase :D:
    ->Se crea el primer programa hecho en java que consiste en un simple "Hola mundo". De toda la vida
    
    ->También hizo una práctica que muestra un menú simple

---
+++ Java/HolaMundo/holaMundo.java
@@ -0,0 +1,8 @@
+public class holaMundo
+{
+    public static void main (String[]arg)
+    {
+        System.out.println("Hola Mundo :D.");
+        System.out.println("You stupid nigg-");
+    }
+}
---
+++ Java/Practicas/P1/menuSimple.java
@@ -0,0 +1,14 @@
+public class menuSimple
+{
+    public static void main (String[]arg)
+    {
+        System.out.println("====================");
+        System.out.println(" Menú de alimentos");
+        System.out.println("====================");
+
+        System.out.println("\n-> Comidas\t\t\t-> Bebidas");
+        System.out.println("* Hamburguesa de pollo\t\t* Refrescos (Cocacola, Fanta ó Sprite)");
+        System.out.println("* Nuggets\t\t\t* Agua de sabor (horchata, jamaica ó tamarindo)");
+        System.out.println("* Papas a la francesa\t\t* Agua natural");
+    }
+}
